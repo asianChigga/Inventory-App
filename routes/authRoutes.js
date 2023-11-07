fff0764be93c87027f071f6e00c50e5fd17fe334
asianChigga <levyng.ng@gmail.com>
2023-11-07 22:31:11 +0530

    added a variable

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,7 +4,7 @@ const {
   registerVendor,
 } = require("../controllers/authControllers");
 const router = express.Router();
-
+const xyz = 123;
 router.route("/user").post(registerUser);
 router.route("/vendor").post(registerVendor);
 // router.route("/login").post()
